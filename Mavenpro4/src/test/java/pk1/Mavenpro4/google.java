edc9785a28f588de0ef739d082a70fab2b1d93bd
ngnghia <nghianv377@gmail.com>
2019-09-27 00:17:09 +0700

    first

---
+++ Mavenpro4/src/test/java/pk1/Mavenpro4/google.java
@@ -0,0 +1,46 @@
+package pk1.Mavenpro4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class google 
+   
+{
+	 private WebDriver driver;	
+	 private static String driverPath = "D:\\TEST TOOLS\\WebDriver\\";
+  @Test
+  public void  testEasy() {
+	  driver.get("https://www.google.com");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Google"));
+  }
+  
+  @Test
+  public void  testEasy2() {
+	  String title = driver.getTitle();	
+		Assert.assertTrue(title.contains("google"));
+  }
+  
+  @Test
+  public void  testEasy3() {
+	  String title = driver.getTitle();	
+		Assert.assertTrue(title.contains("Google"));
+  }
+  @BeforeTest
+  public void beforeTest() {
+	 System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver.exe");
+	  driver = new FirefoxDriver();  
+  }
+
+  @AfterTest
+  public void afterTest() {driver.quit();		
+  }
+}
